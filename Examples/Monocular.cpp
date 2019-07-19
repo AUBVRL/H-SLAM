b8bd89506f96fa7611a30ebdae1efc5ba272460c
gy <gyy01@aub.edu.lb>
2019-07-19 19:12:53 -0400

    added preload and reverse

--- Examples/Monocular.cpp
+++ Examples/Monocular.cpp
@@ -1,31 +1,77 @@
 
 #include "Main.h"
 
-
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-    for (int i =1; i <argc;i++)
+    for (int i = 1; i < argc; i++)
         parseargument(argv[i]);
+
     ValidateInput();
 
     std::shared_ptr<DatasetReader> DataReader = std::make_shared<DatasetReader>(dataset_, sensor_, Path, Path, Path, Path);
-    
-    for (int i = 0; i < DataReader->nImgL; i++)
+
+    if (Reverse)
     {
-        std::shared_ptr<ImageData> Img = std::make_shared<ImageData>();
-        DataReader->getImage(Img, sensor_, i);
+        int temp = Start;
+        Start = End - 1;
+        if (Start >= DataReader->nImgL)
+            Start = DataReader->nImgL - 1;
+        End = temp;
+    }
+
+    std::vector<int> idsToPlay;
+    std::vector<double> timesToPlayAt;
+    for (int i = Start; i >= 0 && i < DataReader->nImgL && linc * i < linc * End; i += linc)
+    {
+        idsToPlay.push_back(i);
+        if (timesToPlayAt.size() == 0)
+        {
+            timesToPlayAt.push_back((double)0);
+        }
+        else
+        {
+            // double tsThis = reader->getTimestamp(idsToPlay[idsToPlay.size()-1]);
+            // double tsPrev = reader->getTimestamp(idsToPlay[idsToPlay.size()-2]);
+            // timesToPlayAt.push_back(timesToPlayAt.back() +  fabs(tsThis-tsPrev)/playbackSpeed);
+        }
+    }
+
+    std::vector<std::shared_ptr<ImageData>> Images;
+    if (Prefetch && Images.empty())
+    {
+        printf("LOADING ALL IMAGES!\n");
+        for (int ii = 0; ii < (int)idsToPlay.size(); ii++)
+        {
+            int i = idsToPlay[ii];
+            std::shared_ptr<ImageData> Img = std::make_shared<ImageData>();
+            DataReader->getImage(Img, sensor_, i);
+            Images.push_back(Img);
+        }
+    }
+
+    for (int ii = 0; ii < (int)idsToPlay.size(); ii++)
+    {
+        int i = idsToPlay[ii];
+        std::shared_ptr<ImageData> Img;
+        if (Prefetch)
+            Img = Images[ii];
+        else
+        {
+            Img = std::make_shared<ImageData>();
+            DataReader->getImage(Img, sensor_, i);
+        }
 
 
+        cv::Mat Dest;
+        if(sensor_ == Stereo)
+            cv::hconcat(Img->ImageL, Img->ImageR, Dest);
+        else if (sensor_ == RGBD)
+            cv::hconcat(Img->ImageL, Img->Depth, Dest);
+        else Dest = Img->ImageL;
 
-        // cv::Mat Dest;
-        // cv::hconcat(Img->ImageL, Img->ImageR, Dest);
-        cv::imshow("Img",Img->ImageL);
+        cv::imshow("Img", Dest);
         cv::waitKey(1);
     }
 
-
-
     return 0;
 }
-
-
